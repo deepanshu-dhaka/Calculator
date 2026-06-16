f6c2510f9581486b1b9a81eefe36d1926c462925
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-16 08:16:26 +0000

    Create a new function operate and 3 new variables
    Created a new function operate that takes an operator and two numbers
    and then calls one of the functions(add, subtract, multiply and divide)
    on the numbers.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let num1 = +prompt("Enter first number:");
+let operator = prompt("Enter operator (+, -, *, /):");
+let num2 = +prompt("Enter second number: ");
+
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,3 +20,19 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        console.log(add(num1, num2));
+    }
+    else if (operator === '-') {
+        subtract(num1, num2);
+    }
+    else if (operator === '*') {
+        multiply(num1, num2);
+    }
+    else if (operator === '/') {
+        divide(num1, num2);
+    }
+}
+
+operate(num1, operator, num2)
