b27e3527cc12535aa1bbf1a3e72c890f2e43bdae
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-21 13:03:33 +0000

    Implement basic features of Calculator
    All the basic features like addition, multiplication, divison,
    subtraction, backspace and AC are done in this commit.

--- script.js
+++ script.js
@@ -1,38 +1,174 @@
-// let num1 = +prompt("Enter first number:");
-// let operator = prompt("Enter operator (+, -, *, /):");
-// let num2 = +prompt("Enter second number: ");
+let num1 = '';
+let operator;
+const operators = ['+', '-', '*', '/'];
+let num2 = '';
+let result;
+let isFirstVaribaleDeclared = false;
+let isSecondVariableDeclared = false;
+let isEqualtoOperatorClicked = false;
 
+let acBtn = document.querySelector("#ac-button");
+acBtn.addEventListener("click", clearTheDisplay
+)
+
+function clearTheDisplay(e) {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    screenDisplay.textContent = '0';
+    isFirstVaribaleDeclared = false;
+    isSecondVariableDeclared = false;
+    isEqualtoOperatorClicked = false;
+}
 
 
 function add(num1, num2) {
-    return num1 + num2;
+    screenDisplay.textContent = num1 + num2;
+    result = num1 + num2;
+    isSecondVariableDeclared = false;
+    return result
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    screenDisplay.textContent = num1 - num2;
+    result = num1 - num2;
+    isSecondVariableDeclared = false;
+    return result
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    screenDisplay.textContent = num1 * num2;
+    result = num1 * num2;
+    isSecondVariableDeclared = false;
+    return result
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    result = num1 / num2;
+    if (result == 'Infinity') {
+        screenDisplay.textContent = "Can't divide by zero!"
+        return;
+    }
+    screenDisplay.textContent = num1 / num2;
+
+    isSecondVariableDeclared = false;
+    return result
 }
 
 function operate(num1, operator, num2) {
     if (operator === '+') {
-        console.log(add(num1, num2));
+        return (add(num1, num2));
     }
     else if (operator === '-') {
-        subtract(num1, num2);
+        return subtract(num1, num2);
     }
     else if (operator === '*') {
-        multiply(num1, num2);
+        return multiply(num1, num2);
     }
     else if (operator === '/') {
-        divide(num1, num2);
+        return divide(num1, num2);
     }
+
 }
 
-operate(num1, operator, num2)
+let screenDisplay = document.querySelector(".screen-container p");
+console.log(screenDisplay.textContent)
+
+let digitBtns = document.querySelectorAll(".digit-btn");
+console.log(digitBtns);
+
+let equaltoBtn = document.querySelector(".equalto-btn");
+equaltoBtn.addEventListener("click", function equaltoBtnHandler(e) {
+    if (num1 && operator && num2) {
+        num1 = operate(+num1, operator, +num2)
+        console.log(num1)
+        num1 = '';
+        num2 = '';
+        operator = '';
+        isFirstVaribaleDeclared = false;
+        isSecondVariableDeclared = false;
+        isEqualtoOperatorClicked = false;
+    }
+
+})
+
+let operatorBtns = document.querySelectorAll(".operator-btn");
+console.log(operatorBtns)
+let isOperatorDeclared = false;
+operatorBtns.forEach((item) =>
+    item.addEventListener("click", function (e) {
+        // if (!isOperatorDeclared) {
+        //     operator = e.target.textContent
+        //     isOperatorDeclared = true;
+        // }
+        // else if (operator && num2 && isOperatorDeclared) {
+        //     operate(+num1, operator, +num2);
+        //     operator = e.target.textContent;
+        //     num1 = result;
+        // }
+        if (isSecondVariableDeclared) {
+            num1 = operate(+num1, operator, +num2)
+            console.log(num1)
+            num2 = " ";
+        }
+
+        if (num1) {
+            operator = e.target.textContent;
+            console.log("operator defined")
+            isFirstVaribaleDeclared = true;
+        }
+
+
+
+    })
+)
+
+
+function clickHandler(e) {
+    // num1 = e.target.textContent;
+    // screenDisplay.textContent = num1;
+    // console.log(num1)
+    getValues(e)
+}
+
+function getValues(e) {
+    if (!isFirstVaribaleDeclared) {
+        let clickedDigit = e.target.textContent;
+        num1 = num1 + clickedDigit;
+        screenDisplay.textContent = num1;
+    }
+    else if (isFirstVaribaleDeclared && num1) {
+        console.log("yay,we'are here")
+        isSecondVariableDeclared = true;
+        let clickedDigit = e.target.textContent;
+        num2 = num2 + clickedDigit;
+        screenDisplay.textContent = num2;
+    }
+    // else if (operator === '=') {
+    //     operate(+num1, operator, +num2)
+    // }
+
+}
+
+digitBtns.forEach((item) =>
+    item.addEventListener("click", clickHandler)
+)
+
+
+let backspaceBtn = document.querySelector("#backspace-btn");
+backspaceBtn.addEventListener("click", function (e) {
+    let len = screenDisplay.textContent.length;
+    console.log(len)
+    if (len) {
+        if (num2) {
+            num2 = num2.slice(0, -1);
+            screenDisplay.textContent = screenDisplay.textContent.slice(0, -1);
+        }
+        else {
+            num1 = num1.slice(0, -1);
+            screenDisplay.textContent = screenDisplay.textContent.slice(0, -1);
+        }
+
+    }
+
+})
